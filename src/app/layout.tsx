058fd3f5f25844d722b5afabaaca98b49a6a7250
Finn Hering <finn@hering.sh>
2024-06-22 18:22:27 +0200

    feat: add mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
 import {AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter';
 import {ThemeProvider} from '@mui/material/styles';
-import theme from '../theme'
+import {Inter} from "next/font/google";
+
+import theme from "@/theme";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -17,14 +18,15 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" style={{"height": "100%"}}>
-        <body style={{"height": "100%", "margin": 0, "padding": 0}}>
-        <AppRouterCacheProvider>
-            <ThemeProvider theme={theme}>
-                {children}
-            </ThemeProvider>
-        </AppRouterCacheProvider>
-        </body>
-        </html>
+            <html lang="en">
+            <body className={inter.className}>
+            <AppRouterCacheProvider>
+                <ThemeProvider theme={theme}>
+                    {children}
+                </ThemeProvider>
+            </AppRouterCacheProvider>
+
+            </body>
+            </html>
     );
 }
