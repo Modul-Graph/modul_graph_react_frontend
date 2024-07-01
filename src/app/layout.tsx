3229e39bbcb7bd90bd579d76b4f2bccf812fbbf4
adriana <adrianad3m3@gmx.de>
2024-07-01 18:41:00 +0200

    stuff i dont understand

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import {AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter';
 import {ThemeProvider} from '@mui/material/styles';
-import {Inter} from "next/font/google";
-
-import theme from "@/theme";
+import theme from '../theme'
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -18,15 +17,14 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-            <html lang="en" style={{height: "100%"}}>
-            <body className={inter.className} style={{height: "100%", margin: 0}}>
-            <AppRouterCacheProvider>
-                <ThemeProvider theme={theme}>
-                    {children}
-                </ThemeProvider>
-            </AppRouterCacheProvider>
-
-            </body>
-            </html>
+        <html lang="en">
+        <body>
+        <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+                {children}
+            </ThemeProvider>
+        </AppRouterCacheProvider>
+        </body>
+        </html>
     );
 }
