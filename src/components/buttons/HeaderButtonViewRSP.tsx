756e263062b95ff618f7190b81be954622c9fafa
adriana <adrianad3m3@gmx.de>
2024-07-01 18:25:40 +0200

    implements buttons for student header

---
+++ src/components/buttons/HeaderButtonViewRSP.tsx
@@ -0,0 +1,7 @@
+import {Button} from "@mui/material";
+
+export const HeaderButtonViewRSP = (): JSX.Element => {
+    return(
+            <Button variant={"contained"} disableElevation={true}>Regelstudienplan anschauen</Button>
+)
+}
