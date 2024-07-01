83a89b8c4cf8a7ca475b3ca64fbaea35a8f15111
adriana <adrianad3m3@gmx.de>
2024-07-01 18:40:07 +0200

    implements Select Dialog

---
+++ src/components/select/SelectDialog.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import {SelectStudyProgram} from "@/components/select/SelectStudyProgram";
+import {SelectSPO} from "@/components/select/SelectSPO";
+
+export const SelectDialog = ({open, setOpen}: SelectInDialogProps) => {
+
+    const handleClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+        if (reason !== 'backdropClick') {
+            setOpen(false);
+        }
+    };
+
+    return (
+            <div>
+                <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
+                    <DialogTitle>Studiengang und Studienprüfungsordnung</DialogTitle>
+                    <DialogContent>
+                        <Box component="form" sx={{display: 'flex', flexWrap: 'wrap'}}>
+                            <SelectStudyProgram/>
+                            <SelectSPO/>
+                        </Box>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleClose}>Ok</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+    );
+}
+
+type SelectInDialogProps = {
+    open: boolean,
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
