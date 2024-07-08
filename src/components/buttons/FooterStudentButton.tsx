e08e89d92d30f5b65f7b935d6e0328ddcf634ef3
adriana <adrianad3m3@gmx.de>
2024-07-08 16:22:37 +0200

    implement custom button for student footer

---
+++ src/components/buttons/FooterStudentButton.tsx
@@ -0,0 +1,7 @@
+import Button from "@mui/material/Button";
+/**
+ * functional component for a custom button for footer
+ * **/
+export const FooterStudentButton = () => {
+    return(<Button variant={"outlined"}>als PDF exportieren</Button>)
+}
