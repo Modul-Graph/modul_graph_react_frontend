10aa26f5678091e5297ea5a2d64d9994f4eff33f
adriana <adrianad3m3@gmx.de>
2024-07-08 16:23:35 +0200

    implement student footer

--- src/app/student_view/page.tsx
+++ src/app/student_view/page.tsx
@@ -1,5 +1,10 @@
 import {StudentHeader} from "@/components/dashboardHeader/StudentHeader";
+import {StudentFooter} from "@/components/dashboardFooter/StudentFooter";
 
 export default function StudentViewPage() {
-    return <StudentHeader/>
+    return (
+            <>
+                <StudentHeader/>
+                <StudentFooter/>
+            </>)
 }
