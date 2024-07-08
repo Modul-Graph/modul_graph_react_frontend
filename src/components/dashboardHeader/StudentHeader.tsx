80b26c128f078692604a10cc12ee66493b7e1d93
adriana <adrianad3m3@gmx.de>
2024-07-08 13:31:16 +0200

    implement generic title and nested grids for student view

--- src/components/dashboardHeader/StudentHeader.tsx
+++ src/components/dashboardHeader/StudentHeader.tsx
@@ -2,5 +2,5 @@ import {GenericHeader} from "@/components/dashboardHeader/GenericHeader";
 import {StudentHeaderGrid} from "@/components/dashboardHeader/StudentHeaderGrid";
 
 export const StudentHeader = () => {
-    return (<GenericHeader child={<StudentHeaderGrid/>}/>)
+    return (<GenericHeader title="individueller Studienplan" child={<StudentHeaderGrid/>}/>)
 }
--- src/components/dashboardHeader/StudentHeaderGrid.tsx
+++ src/components/dashboardHeader/StudentHeaderGrid.tsx
@@ -1,22 +1,18 @@
 import {Grid} from "@mui/material";
-import {HeaderTitle} from "@/components/dashboardHeader/HeaderTitle";
 import {SelectCompetencies} from "@/components/select/SelectCompetencies";
 import {ButtonSelectStudyProgram} from "@/components/buttons/ButtonSelectStudyProgram";
 import {HeaderButtonViewRSP} from "@/components/buttons/HeaderButtonViewRSP";
 
 export const StudentHeaderGrid = () => {
     return(
-            <Grid direction={"row"} rowSpacing={1} columnSpacing={2} alignItems="center" container>
-                            <Grid item>
-                                <HeaderTitle title="individueller Studienplan"/>
-                            </Grid>
-                            <Grid item xs={12} sm={6} md={4} lg={2.2}>
+            <Grid direction={"row"} rowSpacing={1} columnSpacing={2} alignItems="center" xs={10} container item>
+                            <Grid container item xs={12} sm={6} md={4} lg={"auto"}>
                                 <SelectCompetencies></SelectCompetencies>
                             </Grid>
-                            <Grid item xs={12} sm={6} md={3} lg={2.2}>
+                            <Grid container item xs={12} sm={6} md={3} lg={"auto"}>
                                 <HeaderButtonViewRSP/>
                             </Grid>
-                            <Grid item xs={12} sm={6} md={3} lg={2.5}>
+                            <Grid container item xs={12} sm={6} md={3} lg={"auto"}>
                                 <ButtonSelectStudyProgram></ButtonSelectStudyProgram>
                             </Grid>
                         </Grid>
