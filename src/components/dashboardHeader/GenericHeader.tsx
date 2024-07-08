4b0413c07957a800f583f154f0357645f1524178
adriana <adrianad3m3@gmx.de>
2024-07-08 13:30:36 +0200

    implement generic title and nested grids

--- src/components/dashboardHeader/GenericHeader.tsx
+++ src/components/dashboardHeader/GenericHeader.tsx
@@ -1,19 +1,31 @@
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import {Grid} from "@mui/material";
+import {HeaderTitle} from "@/components/dashboardHeader/HeaderTitle";
+
 /**
- * renders child component in header
+ * renders title and child component in header
  * **/
-export const GenericHeader = ({child}:GenericHeaderProps) => {
+export const GenericHeader = ({title, child}: GenericHeaderProps) => {
     return (
-             <Box sx={{flexGrow: 1}}>
+            <Box sx={{flexGrow: 1}}>
                 <AppBar position="static">
                     <Toolbar>
-                        {child}
+                        <Grid container rowSpacing={1} alignItems="center" direction={"row"} >
+
+                            <Grid item>
+                                <HeaderTitle title={title}/>
+                            </Grid>
+                            {child}
+                        </Grid>
                     </Toolbar>
                 </AppBar>
             </Box>
     )
 }
 
-type GenericHeaderProps = {child:React.ReactNode}
+type GenericHeaderProps = {
+    title: string;
+    child: React.ReactNode
+}
