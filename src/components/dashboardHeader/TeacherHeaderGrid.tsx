020ba0375ae79ef82e66dbfe26ee57e1ae958c5c
adriana <adrianad3m3@gmx.de>
2024-07-08 13:31:35 +0200

    implement generic title and nested grids for teacher view

--- src/components/dashboardHeader/TeacherHeaderGrid.tsx
+++ src/components/dashboardHeader/TeacherHeaderGrid.tsx
@@ -2,27 +2,22 @@ import {Grid} from "@mui/material";
 import {SelectCompetencies} from "@/components/select/SelectCompetencies";
 import {HeaderButtonGroup} from "@/components/buttons/HeaderButtonGroup";
 import {ButtonSelectStudyProgram} from "@/components/buttons/ButtonSelectStudyProgram";
-import {HeaderTitle} from "@/components/dashboardHeader/HeaderTitle";
 import {HeaderButtonAnalysis} from "@/components/buttons/HeaderButtonAnalysis";
 
 export const TeacherHeaderGrid = () => {
 
     return(
-           <Grid direction={"row"} rowSpacing={1} columnSpacing={2} alignItems="center" container>
-                            <Grid item>
-                                <HeaderTitle title="Studienverlaufsplan"/>
-
-                            </Grid>
-                            <Grid item xs={12} sm={6} md={4} lg={2.2}>
+           <Grid direction={"row"} rowSpacing={1} columnSpacing={2} alignItems="center" container item xs={10}>
+                            <Grid container item xs={12} sm={6} md={4} lg={"auto"}>
                                 <SelectCompetencies></SelectCompetencies>
                             </Grid>
-                            <Grid item xs={12} sm={8} md={6} lg={4}>
+                            <Grid container item xs={12} sm={8} md={6} lg={"auto"}>
                                 <HeaderButtonGroup/>
                             </Grid>
-                            <Grid item xs={12} sm={6} md={3} lg={1.7}>
+                            <Grid container item xs={12} sm={6} md={3} lg={"auto"}>
                                 <HeaderButtonAnalysis/>
                             </Grid>
-                            <Grid item xs={12} sm={6} md={3} lg={2.5}>
+                            <Grid container item xs={12} sm={6} md={3} lg={"auto"}>
                                 <ButtonSelectStudyProgram></ButtonSelectStudyProgram>
                             </Grid>
                         </Grid>
