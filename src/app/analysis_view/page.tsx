af9f42f2738cafd5bd3d5969dd7a59ee7f2ee327
adriana <adrianad3m3@gmx.de>
2024-07-09 18:54:55 +0200

    implements Machbarkeitsanlyse view

---
+++ src/app/analysis_view/page.tsx
@@ -0,0 +1,13 @@
+import {AnalysisHeader} from "@/components/dashboardHeader/AnalysisHeader";
+import {AnalysisFooter} from "@/components/dashboardFooter/AnalysisFooter";
+/**
+ * renders diff view (visualizes changes made during a session)
+ * */
+
+export default function AnalysisViewPage() {
+    return (
+            <>
+                <AnalysisHeader/>
+                <AnalysisFooter/>
+            </>)
+}
---
+++ src/components/dashboardHeader/AnalysisHeader.tsx
@@ -0,0 +1,8 @@
+import {GenericHeader} from "@/components/dashboardHeader/GenericHeader";
+
+/**
+ * renders header for difference view
+ * */
+export const AnalysisHeader = () => {
+    return (<GenericHeader title={"Machbarkeitsanalyse"} child={<></>}/>)
+}
