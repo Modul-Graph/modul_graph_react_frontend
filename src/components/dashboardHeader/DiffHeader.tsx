afb52f973490fbd9df855e73bea2be7b13975e7b
adriana <adrianad3m3@gmx.de>
2024-07-09 18:55:23 +0200

    implements difference view

---
+++ src/components/dashboardHeader/DiffHeader.tsx
@@ -0,0 +1,8 @@
+import {GenericHeader} from "@/components/dashboardHeader/GenericHeader";
+
+/**
+ * renders header for analysis view
+ * */
+export const DiffHeader = () => {
+    return (<GenericHeader title={"Differenzansicht"} child={<></>}/>)
+}
