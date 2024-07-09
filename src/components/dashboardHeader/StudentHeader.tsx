093f85084667dbe7795d4ff91293acf60850a8e7
adriana <adrianad3m3@gmx.de>
2024-07-09 18:09:18 +0200

    comments

--- src/components/dashboardHeader/StudentHeader.tsx
+++ src/components/dashboardHeader/StudentHeader.tsx
@@ -1,6 +1,8 @@
 import {GenericHeader} from "@/components/dashboardHeader/GenericHeader";
 import {StudentHeaderGrid} from "@/components/dashboardHeader/StudentHeaderGrid";
-
+/**
+ * renders header for the student view
+ * **/
 export const StudentHeader = () => {
     return (<GenericHeader title="individueller Studienplan" child={<StudentHeaderGrid/>}/>)
 }
--- src/components/dashboardHeader/StudentHeaderGrid.tsx
+++ src/components/dashboardHeader/StudentHeaderGrid.tsx
@@ -2,7 +2,9 @@ import {Grid} from "@mui/material";
 import {SelectCompetencies} from "@/components/select/SelectCompetencies";
 import {ButtonSelectStudyProgram} from "@/components/buttons/ButtonSelectStudyProgram";
 import {HeaderButtonViewRSP} from "@/components/buttons/HeaderButtonViewRSP";
-
+/**
+ * responsive header grid layout for the student view with interactive components
+ * **/
 export const StudentHeaderGrid = () => {
     return(
             <Grid direction={"row"} rowSpacing={1} columnSpacing={2} alignItems="center" xs={10} container item>
