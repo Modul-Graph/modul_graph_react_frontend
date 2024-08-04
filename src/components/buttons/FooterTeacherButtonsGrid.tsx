2bcde815603d7721a840384b2b0386aee9aa8402
adriana <adrianad3m3@gmx.de>
2024-08-04 15:26:14 +0200

    remove diff view

--- src/components/buttons/FooterTeacherButtonsGrid.tsx
+++ src/components/buttons/FooterTeacherButtonsGrid.tsx
@@ -7,9 +7,6 @@ export const FooterTeacherButtonsGrid = () => {
     return(
             <Grid container item direction={"row"} rowSpacing={1} width={"unset"} spacing={1}>
                 <Grid item>
-                    <Button variant="outlined">Differenzansicht</Button>
-                </Grid>
-                <Grid item>
                     <Button variant="contained" color="primary">Änderungen speichern</Button>
                 </Grid>
             </Grid>
