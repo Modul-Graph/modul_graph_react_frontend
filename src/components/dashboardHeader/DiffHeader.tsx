d134e0c5366954a9c2439018090b24f95b1f346e
adriana <adrianad3m3@gmx.de>
2024-08-04 15:26:14 +0200

    remove diff view

--- src/components/dashboardHeader/DiffHeader.tsx
+++
@@ -1,8 +0,0 @@
-import {GenericHeader} from "@/components/dashboardHeader/GenericHeader";
-
-/**
- * renders header for analysis view
- * */
-export const DiffHeader = () => {
-    return (<GenericHeader title={"Differenzansicht"} child={<></>}/>)
-}
