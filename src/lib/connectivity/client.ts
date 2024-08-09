dfa3012825e431e1748eea4b16e9763c40a5d866
Finn Hering <finn@hering.sh>
2024-08-09 14:43:05 +0200

    feat: add timetable sc

--- src/lib/connectivity/client.ts
+++ src/lib/connectivity/client.ts
@@ -1,6 +1,7 @@
 import {Zodios} from "@zodios/core";
 import doabilityResponseSchema from "@/lib/zod/doabilityResponseSchema";
 import {getClientEnvironment} from "@/lib/zod/environment";
+import {competenceTimeTableSchema} from "@/lib/zod/competenceTimeTableSchema";
 
 
 const API_URL = getClientEnvironment().NEXT_PUBLIC_API_URL
@@ -13,7 +14,13 @@ const apiClient = new Zodios(
         requestFormat: "json",
         response: doabilityResponseSchema,
         alias: "getDoability"
-    }]
+    }, {
+        method: "get",
+        path: "/sc/competence/:standardCurriculum",
+        response: competenceTimeTableSchema,
+        alias: "getCompetenceTimeTable"
+    }
+    ]
 )
 
 export default apiClient
---
+++ src/lib/zod/competenceTimeTableSchema.ts
@@ -0,0 +1,25 @@
+import {z} from "zod";
+
+const competenceSchema = z.string().min(1, "Competence must not be empty")
+const moduleNameSchema = z.string().min(1, "Module/WPF name must not be empty")
+
+export const competenceTimeTableSchema = z.object({
+    pflichtmodule: z.array(z.object({
+        name: moduleNameSchema,
+        competences: z.array(competenceSchema),
+        semester: z.number().int().min(1, "Semester must be at least 1"),
+    })),
+    WPF: z.array(
+        z.object({
+            name: moduleNameSchema,
+            modules: z.array(z.object({
+                name: moduleNameSchema,
+                competences: z.array(competenceSchema)
+            })),
+            semesters: z.array(z.number().int().min(1, "Semester must be at least 1"))
+        })
+    )
+})
+
+export type CompetenceTimeTableType = z.infer<typeof competenceTimeTableSchema>
+
