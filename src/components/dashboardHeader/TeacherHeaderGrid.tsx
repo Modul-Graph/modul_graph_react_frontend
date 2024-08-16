1a3d7cd2af3d1eeb264fe5bc9286576344e42878
adriana <adrianad3m3@gmx.de>
2024-08-16 10:57:15 +0200

    implement teacher header without competency drop down

--- src/components/dashboardHeader/TeacherHeaderGrid.tsx
+++ src/components/dashboardHeader/TeacherHeaderGrid.tsx
@@ -1,8 +1,7 @@
 import {Grid} from "@mui/material";
-import {SelectCompetencies} from "@/components/select/SelectCompetencies";
 import {HeaderButtonGroup} from "@/components/buttons/HeaderButtonGroup";
 import {ButtonSelectStudyProgram} from "@/components/buttons/ButtonSelectStudyProgram";
-import {HeaderButtonAnalysis} from "@/components/buttons/HeaderButtonAnalysis";
+
 
 /**
  * responsive header grid layout for the teacher view with interactive components
@@ -12,18 +11,15 @@ export const TeacherHeaderGrid = () => {
 
     return(
            <Grid direction={"row"} rowSpacing={1} columnSpacing={2} alignItems="center" container item xs={10}>
-                            <Grid container item xs={12} sm={6} md={4} lg={"auto"}>
-                                <SelectCompetencies></SelectCompetencies>
-                            </Grid>
+
                             <Grid container item xs={12} sm={8} md={6} lg={"auto"}>
                                 <HeaderButtonGroup/>
                             </Grid>
-                            <Grid container item xs={12} sm={6} md={3} lg={"auto"}>
-                                <HeaderButtonAnalysis/>
-                            </Grid>
+
                             <Grid container item xs={12} sm={6} md={3} lg={"auto"}>
                                 <ButtonSelectStudyProgram></ButtonSelectStudyProgram>
                             </Grid>
+
                         </Grid>
     )
 }
