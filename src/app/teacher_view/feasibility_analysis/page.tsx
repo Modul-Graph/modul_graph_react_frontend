89ac28d04d57fccc30a6b2906b4bbbca769c12cf
adriana <adrianad3m3@gmx.de>
2024-08-16 11:11:15 +0200

    move feasibility page

--- src/app/teacher_view/feasibility_analysis/page.tsx
+++ src/app/teacher_view/feasibility_analysis/page.tsx
@@ -1,7 +1,7 @@
 import {AnalysisHeader} from "@/components/dashboardHeader/AnalysisHeader";
 import {AnalysisFooter} from "@/components/dashboardFooter/AnalysisFooter";
 /**
- * renders diff view (visualizes changes made during a session)
+ * renders feasibility analysis (visualizes if modules fit the study program)
  * */
 
 export default function AnalysisViewPage() {
