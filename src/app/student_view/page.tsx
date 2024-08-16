469cbb331426aa6a740f6af2f809738a045445c3
adriana <adrianad3m3@gmx.de>
2024-08-16 11:08:16 +0200

    implement loading screen, fix overflow

--- src/app/student_view/page.tsx
+++ src/app/student_view/page.tsx
@@ -4,21 +4,29 @@ import {CustomGraph} from "@/components/studyProgramGraph/CustomGraph";
 import {edges, nodes} from "@/components/studyProgramGraph/MockData";
 import Box from "@mui/material/Box";
 import {Stack} from "@mui/material";
+import {GraphSkeleton} from "@/components/placeholder/GraphSkeleton";
+
 /**
  * renders student view with functionalities and module studyProgramGraph
  * */
 export default function StudentViewPage() {
+    const isLoading = true;
+
     return (
             <Box height={1}>
                 <Stack height={1}>
                     <StudentHeader/>
                     <Box
                             flex={9999999999999999}
-                            sx={{position: "relative"}} //
+                            sx={{position: "relative", overflow: "hidden"}} //
                             display={"block"}
 
                     >
-                        <CustomGraph nodes={nodes} edges={edges}></CustomGraph>
+                        {isLoading ?
+                                <GraphSkeleton/>
+                                : <CustomGraph nodes={nodes} edges={edges}></CustomGraph>
+                        }
+
                     </Box>
 
                     <StudentFooter/>
