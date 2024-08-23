0cccb24984fad090cb16e6f7a210b51cbd7d5d35
adriana <adrianad3m3@gmx.de>
2024-08-23 14:30:33 +0200

    mock stuff, need to rebase on finns branch to use schema?

---
+++ src/components/competencyGraph/MockData2.tsx
@@ -0,0 +1,58 @@
+//todo: delete schema
+import {z} from "zod";
+
+const competenceSchema = z.string().min(1, "Competence must not be empty")
+const moduleNameSchema = z.string().min(1, "Module/WPF name must not be empty")
+
+export const competenceTimeTableSchema = z.object({
+    pflichtmodule: z.array(z.object({
+        name: moduleNameSchema,
+        competences: z.array(competenceSchema),
+        semester: z.number().int().min(1, "Semester must be at least 1"),
+    })),
+    WPF: z.array(
+        z.object({
+            name: moduleNameSchema,
+            modules: z.array(z.object({
+                name: moduleNameSchema,
+                competences: z.array(competenceSchema)
+            })),
+            semesters: z.array(z.number().int().min(1, "Semester must be at least 1"))
+        })
+    )
+})
+
+export type CompetenceTimeTableType = z.infer<typeof competenceTimeTableSchema>
+
+export type WPFEntry = CompetenceTimeTableType["WPF"][0]
+
+
+
+export const data = [
+    {
+        name: 'ModuleArea A',
+        modules: [
+            {
+                name: 'Submodule A1',
+                competences: ['Competence 1', 'Competence 2']
+            },
+            {
+                name: 'Submodule A2',
+                competences: ['Competence 2', 'Competence 3']
+            }
+        ],
+        semesters: [1, 2]
+    },
+    {
+        name: 'ModuleArea B',
+        modules: [
+            {
+                name: 'Submodule B1',
+                competences: ['Competence 3', 'Competence 4']
+            }
+        ],
+        semesters: [1]
+    }
+];
+
+
