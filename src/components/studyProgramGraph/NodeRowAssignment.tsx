d79cddce2e4bfe64b29c971ffb7a0d030e4b8c94
adriana <adrianad3m3@gmx.de>
2024-08-29 15:41:03 +0200

    nodes for 7 semesters

--- src/components/studyProgramGraph/NodeRowAssignment.tsx
+++ src/components/studyProgramGraph/NodeRowAssignment.tsx
@@ -27,7 +27,6 @@ export const convertNodes = (_nodes: CustomNode[]) => {
         // assign node a rowID property based on position in columnNodes[]
         columnNodes.forEach((node, rowIndex) => {
             node.rowID = rowIndex + 3;
-            console.log(node)
 
         });
     });
@@ -36,11 +35,11 @@ export const convertNodes = (_nodes: CustomNode[]) => {
     nodes.push({id: 'sem-01', label: '1', sem: 1, rowID: 1});
     nodes.push({id: 'sem-02', label: '2', sem: 2, rowID: 1});
     nodes.push({id: 'sem-03', label: '3', sem: 3, rowID: 1});
-    nodes.push({id: 'sem-04', label: '4', sem: 4, rowID: 1})
+    nodes.push({id: 'sem-04', label: '4', sem: 4, rowID: 1});
+    nodes.push({id: 'sem-05', label: '5', sem: 5, rowID: 1});
+    nodes.push({id: 'sem-06', label: '6', sem: 6, rowID: 1});
+    nodes.push({id: 'sem-07', label: '7', sem: 7, rowID: 1});
 
-    //nodes.push({id: 'sem-05', label: '5', sem: 5, rowID: 1});
-    //nodes.push({id: 'sem-06', label: '6', sem: 6, rowID: 1});
-    //nodes.push({id: 'sem-07', label: '7', sem: 7, rowID: 1})
     return nodes; // return modified nodes array
 };
 
