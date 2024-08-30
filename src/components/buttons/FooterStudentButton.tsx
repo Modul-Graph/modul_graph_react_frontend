3d9ff4f3c48738411423731e030929d20eb2a7f6
adriana <adrianad3m3@gmx.de>
2024-08-30 15:37:15 +0200

    implement createPDF functionality, disable panning interaction with usehook

--- src/components/buttons/FooterStudentButton.tsx
+++ src/components/buttons/FooterStudentButton.tsx
@@ -1,7 +1,14 @@
 import Button from "@mui/material/Button";
+
 /**
- * functional component for a custom button for footer
+ * functional component for a custom button for student view footer
  * **/
-export const FooterStudentButton = () => {
-    return(<Button variant={"outlined"}>als PDF exportieren</Button>)
+export const FooterStudentButton = ({onClick}: FooterStudentButtonProps) => {
+
+    // export PDF function in student_view page
+    return(<Button variant={"outlined"} onClick={onClick}>als PDF exportieren</Button>)
+}
+
+export type FooterStudentButtonProps = {
+    onClick?: () => Promise<void>
 }
